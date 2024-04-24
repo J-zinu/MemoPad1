d1b966a7c0d354d98e3f5334db023b5e98fa6771
J-zinu <wjswlsdn4053@naver.com>
2024-04-24 10:35:42 +0900

    db까지 추가했음

---
+++ MemoPad1/MemoData.h
@@ -0,0 +1,16 @@
+//
+//  MemoData.h
+//  MemoPad1
+//
+//  Created by JinWoo Jeong on 4/24/24.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface MemoData : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ MemoPad1/MemoData.m
@@ -0,0 +1,12 @@
+//
+//  MemoData.m
+//  MemoPad1
+//
+//  Created by JinWoo Jeong on 4/24/24.
+//
+
+#import "MemoData.h"
+
+@implementation MemoData
+
+@end
